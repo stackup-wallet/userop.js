c6fca59d6ca6ab0ab78d79964bacc7e27347b083
hazim-j <hazim.jumali@gmail.com>
2023-08-29 11:49:22 +1000

    Remove redundant estimateCreationGas from gasLimit middleware

--- src/preset/middleware/gasLimit.ts
+++ src/preset/middleware/gasLimit.ts
@@ -1,4 +1,4 @@
-import { BigNumberish, BytesLike, ethers } from "ethers";
+import { BigNumberish, ethers } from "ethers";
 import { OpToJSON } from "../../utils";
 import { UserOperationMiddlewareFn } from "../../types";
 
@@ -11,28 +11,9 @@ interface GasEstimate {
   verificationGas: BigNumberish;
 }
 
-const estimateCreationGas = async (
-  provider: ethers.providers.JsonRpcProvider,
-  initCode: BytesLike
-): Promise<ethers.BigNumber> => {
-  const initCodeHex = ethers.utils.hexlify(initCode);
-  const factory = initCodeHex.substring(0, 42);
-  const callData = "0x" + initCodeHex.substring(42);
-  return await provider.estimateGas({
-    to: factory,
-    data: callData,
-  });
-};
-
 export const estimateUserOperationGas =
   (provider: ethers.providers.JsonRpcProvider): UserOperationMiddlewareFn =>
   async (ctx) => {
-    if (ethers.BigNumber.from(ctx.op.nonce).isZero()) {
-      ctx.op.verificationGasLimit = ethers.BigNumber.from(
-        ctx.op.verificationGasLimit
-      ).add(await estimateCreationGas(provider, ctx.op.initCode));
-    }
-
     const est = (await provider.send("eth_estimateUserOperationGas", [
       OpToJSON(ctx.op),
       ctx.entryPoint,
